ed24f5f988fa8b6010be12a97137333940b91e44
Dax Gerber <dax.gerber@gmail.com>
2023-09-29 09:49:19 -0600

    First Commit

---
+++ TechStartupApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TechStartupApp
+//
+//  Created by Dax Gerber on 9/29/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
